72595772e82872cfbbe73ad6e6cafb10ccbffff2
Sonu Abraham <sonu.abraham@aremedia.com.au>
2023-02-10 15:24:57 +1100

    Updated code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RisingSun from "./risingSun/risingSunForm";
+import RisingSun from "./RisingSun/RisingSunForm";
 
 ReactDOM.createRoot(document.getElementById("rising-sun-root")).render(
   <React.StrictMode>
