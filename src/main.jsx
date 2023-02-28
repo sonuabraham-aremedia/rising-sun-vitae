23ae593441966c33aa62217ffbf93d4f7181c622
Sonu Abraham <sonu.abraham@aremedia.com.au>
2023-02-28 17:30:52 +1100

    Updatd case in paths.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RisingSun from "./risingSun/risingSunForm";
+import RisingSun from "./RisingSun/RisingSunForm";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
