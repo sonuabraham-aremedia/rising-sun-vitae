a7d793d94206095aab775b510f0d77e72ad8af51
Sonu Abraham <sonu.abraham@aremedia.com.au>
2023-03-02 14:45:19 +1100

    Fixed case sensitive paths.

--- src/RisingSun/RisingSunForm.tsx
+++ src/RisingSun/RisingSunForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, SyntheticEvent } from "react";
-import "./risingSunForm.css";
+import "./RisingSunForm.css";
 import {
   ISailthruClient,
   IWindowWithSailthru,
